f9e9dc0ca9155dd0d0c5845a8383cf194eb55c76
hiro <168133165+st4rxtw@users.noreply.github.com>
2026-08-01 23:18:18 -0400

    Update GLTexture.cpp
    #whoops

--- Include/Render/OpenGL/GLTexture.cpp
+++ Include/Render/OpenGL/GLTexture.cpp
@@ -54,7 +54,7 @@ namespace Render
 		// load texture from a file
 		std::string resolvedPath = Utils::Paths::Resolve(path);
 
-		stbi_set_flip_vertically_on_load(1);
+		stbi_set_flip_vertically_on_load(0);
 
 		int width = 0;
 		int height = 0;
